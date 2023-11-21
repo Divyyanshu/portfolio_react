9258286ea954e372922072dcbbf28d7405843aff
divyanshu tailor <128120812+divyyuanshutailor@users.noreply.github.com>
2023-11-21 17:55:46 +0530

    add cards

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -1,23 +1,32 @@
 import React from 'react';
 import './Services.css';
 import Resume from '../../Components/Services/resume 0.2.pdf'
-
+import Cards from '../Cards/Cards';
+import Heartemoji from '../../img/heartemoji.png'
+import Glasess from '../../img/glasses.png'
 const Services = () => {
   return (
     <div className="services">
-    <div className="awesome">
-    <span>My Awesome</span>
-    <span>Services</span>
-    <span>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Porro magnam
-    <br />
-consectetur adipisicing elit. Dolor, porro.</span>
-<a href={Resume} download><button className="button s-button">
-Download CV
-</button></a>
-    </div>
-    <div className="cards">
-    i am rifht side
-    </div>
+      <div className="awesome">
+        <span>My Awesome</span>
+        <span>Services</span>
+        <span>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Porro magnam
+          <br />
+          consectetur adipisicing elit. Dolor, porro.</span>
+        <a href={Resume} download><button className="button s-button">
+          Download CV
+        </button></a>
+      </div>
+      /*rightside*/
+      <div style={{left : '18rem'}}>
+        <div>
+          <Cards
+            emoji={Heartemoji}
+            heading={"Design"}
+            details={"Figma, Adobe Xd , PhotoShop , Illustrator"}
+          />
+        </div>
+      </div>
     </div>
   )
 }
